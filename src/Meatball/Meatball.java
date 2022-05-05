01da2757c99b190c97e7a02addfb9f4e5c6606e1
hoangnhattan0907 <69751900+hoangnhattan0907@users.noreply.github.com>
2022-05-06 02:37:09 +0700

    Meatball option decorators

--- src/Meatball/Meatball.java
+++ src/Meatball/Meatball.java
@@ -5,19 +5,19 @@ public abstract class Meatball {
     
     public abstract double cost();
 
-    public String getDesciption(){
+    public String getDescription(){
         return description;
     }
 
-    public void prepareDough(String type){
-        System.out.println("Chuẩn bị " + "thịt viên"+ type );
+    public void prepareDough(String type) {
+        System.out.println("Preparing "+ type + " meatball");
     }
 
     public void cookDough(String type) {
-        System.out.println("Đang nấu " + type + " thịt viên");
+        System.out.println("Cooking " + type + " meatball");
     }
 
     public void dishUp(String type) {
-        System.out.println(" " + type + " meatball on the dish");
+        System.out.println("Put " + type + " meatball on the dish");
     }
 }
